b18376562af6212fb5af81c0ff9672530039eadc
Bastien Housiaux <bastienhousiaux@gmail.com>
2018-08-28 16:13:39 +0200

    new

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
-const expres=require("express");
+const express=require("express");
+
+const app=express();
+console.log("app");
+require("./routes");
 
 app.listen(3000,()=>{
     console.log("server started on port 3000");
